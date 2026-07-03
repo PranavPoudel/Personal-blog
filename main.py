cfceed7609f2a00828d35b8283e764b2db3c1de6
PranavPoudel <pranavpoudel22@gmail.com>
2026-07-03 08:31:47 +0545

    add: addded a context manager for the app

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI 
 from database import db_conn
 from auth import router as auth_router
 from routes.articles import router as articles_router
 from routes.admin import router as admin_router
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    #this will run exactly once when the server starts
+    db_conn()
+    yield
+    #will run once when server stops, here is where we will close resources, (for sqlite, leaving empty is fine)
+
+app = FastAPI(lifespan= lifespan)
 
 #initializing the database
 db_conn()
